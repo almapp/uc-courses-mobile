cf8197d3027cadb3e1c31763214b19b6153cd3cc
Patricio López <lopezjuripatricio@gmail.com>
2016-01-02 15:44:58 -0300

    prepare schedule view

--- app/pages/scheduler/scheduler.ts
+++ app/pages/scheduler/scheduler.ts
@@ -1,10 +1,21 @@
-import {Page} from "ionic-framework/ionic";
+import {Page, NavController} from "ionic-framework/ionic";
+
+import {Course} from "../../models/course";
+import {CoursesProvider, DAYS, MODULES} from "../../providers/courses";
+import {CourseItem} from "../../components/course-item/course-item";
 
 @Page({
-  templateUrl: "build/pages/scheduler/scheduler.html",
+    templateUrl: "build/pages/scheduler/scheduler.html",
+    directives: [CourseItem],
+    providers: [CoursesProvider],
 })
 export class SchedulerPage {
-  constructor() {
-      // ...
-  }
+    days: string[] = DAYS;
+
+    constructor(
+        private provider: CoursesProvider,
+        private nav: NavController) {
+
+
+    }
 }
