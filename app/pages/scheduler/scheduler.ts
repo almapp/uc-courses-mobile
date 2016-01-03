0f02c75980b429f4c0cfb5b2953052a7b9067305
Patricio López <lopezjuripatricio@gmail.com>
2016-01-03 12:04:46 -0300

    allow create of schedules

--- app/pages/scheduler/scheduler.ts
+++ app/pages/scheduler/scheduler.ts
@@ -1,4 +1,4 @@
-import {Page, NavController} from "ionic-framework/ionic";
+import {Page, NavController, Platform, Popup} from "ionic-framework/ionic";
 
 import {Course} from "../../models/course";
 import {SchedulesProvider, Schedule} from "../../providers/schedules";
@@ -13,11 +13,47 @@ export class SchedulerPage {
     schedules: Schedule[];
 
     constructor(
-        private manager: SchedulesProvider,
-        private nav: NavController) {
+        private platform: Platform,
+        private nav: NavController,
+        private popup: Popup,
+        private manager: SchedulesProvider) {
 
-        this.manager.load().then(schedules => {
+        this.manager.loadAll().then(schedules => {
             this.schedules = schedules.sort((a, b) => a.position - b.position);
+        });
+    }
+
+    newScheduler() {
+        return this.popup.prompt({
+            title: "Nuevo horario",
+            template: "Ingresa un nombre para este",
+            inputPlaceholder: "Nombre",
+            okText: "Guardar",
+            okType: "secondary",
+        }).then((name: string) => {
+            const repeated = this.schedules.some(sch => {
+                return sch.name === name;
+            });
+            if (repeated) {
+                throw new Error("Horario con nombre repetido");
+            } else {
+                return name;
+            }
+        }).then(name => {
+            const order = this.schedules[0].position + 1;
+            return this.manager.create(name, order);
+        }).then(schedule => {
+            this.schedules.push(schedule);
+        }).catch((err: Error) => {
+            // FIXME: Error: nav controller actively transitioning
+            // return this.alertRepeatedSchedule(err.name);
+        });
+    }
+
+    alertRepeatedSchedule(body: string) {
+        return this.popup.alert({
+            title: "Problema:",
+            template: body,
         });
     }
 }
--- app/providers/schedules.ts
+++ app/providers/schedules.ts
@@ -1,5 +1,6 @@
 import {Storage, SqlStorage} from "ionic-framework/ionic";
 import {Injectable} from "angular2/core";
+import {createHash} from "crypto";
 
 import {Course, ScheduleSchema} from "../models/course";
 
@@ -50,37 +51,70 @@ export class Schedule {
     }
 }
 
+export interface Schedules {
+    [ Identifier: string ]: Schedule;
+}
+
 @Injectable()
 export class SchedulesProvider {
     database: string;
     storage: Storage;
-    schedules: Schedule[];
+
+    schedules: Schedules;
+    names: string[];
 
     constructor() {
         this.database = "buscacursos-uc";
         this.storage = new Storage(<any>SqlStorage, { name: this.database });
-
-        // Default schedule
-        // this.storage.get("schedules").then(result => {
-        //     this.schedules = (result) ? JSON.parse(result) : [new Schedule("Propio", 0)];
-        // });
-
-        // Ensure schedule list;
-        // this.storage.get("SCHEDULES").then(result => {
-        //     if (!result) {
-        //         this.storage.set("SCHEDULES", JSON.stringify(["Personal"]));
-        //     }
-        // });
+        this.schedules = {};
     }
 
-    load(force=false): Promise<Schedule[]> {
-        if (force || !this.schedules) {
-            return this.storage.get("schedules").then(result => {
-                this.schedules = (result) ? JSON.parse(result) : [new Schedule("Propio", 0)];
-                return this.schedules;
-            });
-        } else {
-            return Promise.resolve(this.schedules);
-        }
+    static nameToID(name: string): string {
+        return createHash("md5").update(name).digest("hex");
+    }
+
+    save(schedule: Schedule): Promise<void> {
+        return this.storage.set(SchedulesProvider.nameToID(schedule.name), JSON.stringify(schedule));
+    }
+
+    create(name: string, position?: number, courses?: Course[]): Promise<Schedule> {
+        const schedule = new Schedule(name, position || 0, courses);
+        return this.save(schedule).then(() => {
+            // Save object
+            return this.loadNames();
+        }).then(names => {
+            // Save name
+            names.push(name);
+            this.names = names;
+            return this.storage.set("NAMES", JSON.stringify(names));
+        }).then(() => {
+            // Return on success
+            return schedule;
+        });
+    }
+
+    loadNames(): Promise<string[]> {
+        return (this.names) ? Promise.resolve(this.names) : this.storage.get("NAMES").then(names => {
+            this.names = (names && names.length) ? JSON.parse(names) : ["default"];
+            return this.names;
+        });
+    }
+
+    loadSchedule(name: string): Promise<Schedule> {
+        const ID = SchedulesProvider.nameToID(name);
+        return (this.schedules[ID]) ? Promise.resolve(this.schedules[ID]) : this.storage.get(ID).then(schedule => {
+            if (schedule) {
+                this.schedules[ID] = JSON.parse(schedule);
+            } else if (name === "default") {
+                this.schedules[ID] = new Schedule("Propio", 0);
+            }
+            return this.schedules[ID];
+        });
+    }
+
+    loadAll(): Promise<Schedule[]> {
+        return this.loadNames().then(names => {
+            return Promise.all(names.map(name => this.loadSchedule(name)));
+        });
     }
 }
