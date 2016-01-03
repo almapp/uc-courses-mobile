2364ca973a51e47b62b260eb01f0ef7eb012c6fa
Patricio López <lopezjuripatricio@gmail.com>
2016-01-02 23:04:19 -0300

    move schedule view to own component

--- app/components/schedule-view/schedule-view.ts
+++ app/components/schedule-view/schedule-view.ts
@@ -1,17 +1,41 @@
-import {Component} from "angular2/core";
+import {Component, Input} from "angular2/core";
+import {Segment, SegmentButton} from "ionic-framework/ionic";
 
-import {Course} from "../../models/course";
-import {CoursesProvider, Period, Campus} from "../../providers/courses";
+import {Course, DAYS} from "../../models/course";
+import {CoursesProvider} from "../../providers/courses";
 import {SchedulesProvider, Schedule} from "../../providers/schedules";
+
+const WEEKDAYS = [
+    "D",
+    "L",
+    "M",
+    "W",
+    "J",
+    "V",
+    "S",
+];
 
 @Component({
     selector: "schedule-view",
     templateUrl: "build/components/schedule-view/schedule-view.html",
-    directives: [],
+    directives: [Segment, SegmentButton],
     providers: [CoursesProvider, SchedulesProvider],
 })
 export class ScheduleView {
+    @Input() schedule: Schedule;
+
+    current: string;
+    days: string[] = DAYS;
+
     constructor(private provider: CoursesProvider, private manager: SchedulesProvider) {
-        // ...
+        this.current = this.today;
+    }
+
+    get today(): string {
+        return WEEKDAYS[new Date().getDay()];
+    }
+
+    selectDay(day: string) {
+        this.current = day;
     }
 }
--- app/pages/scheduler/scheduler.ts
+++ app/pages/scheduler/scheduler.ts
@@ -1,48 +1,23 @@
 import {Page, NavController} from "ionic-framework/ionic";
 
 import {Course} from "../../models/course";
-import {DAYS, MODULES} from "../../providers/courses";
 import {SchedulesProvider, Schedule} from "../../providers/schedules";
-import {CourseItem} from "../../components/course-item/course-item";
 import {ScheduleView} from "../../components/schedule-view/schedule-view";
 
 @Page({
     templateUrl: "build/pages/scheduler/scheduler.html",
-    directives: [CourseItem],
+    directives: [ScheduleView],
     providers: [SchedulesProvider],
 })
 export class SchedulerPage {
-    days: string[] = DAYS;
-    current: string;
     schedules: Schedule[];
-
-    static WEEKDAYS = [
-        "D",
-        "L",
-        "M",
-        "W",
-        "J",
-        "V",
-        "S",
-    ];
 
     constructor(
         private manager: SchedulesProvider,
         private nav: NavController) {
 
-        const today = new Date();
-        this.current = this.today;
-
         this.manager.load().then(schedules => {
             this.schedules = schedules.sort((a, b) => a.position - b.position);
         });
-    }
-
-    get today(): string {
-        return SchedulerPage.WEEKDAYS[new Date().getDay()];
-    }
-
-    selectDay(day: any) {
-        this.current = day;
     }
 }
