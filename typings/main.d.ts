5fb2580926eff271600e4d463835ba2c83350153
Patricio López <lopezjuripatricio@gmail.com>
2016-03-02 01:27:33 -0300

    upgrade dev dependencies

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="main/ambient/node/node.d.ts" />
-/// <reference path="main/definitions/es6-promise/es6-promise.d.ts" />
